a1fa5c4a051a3c49807f7f787c1a93a3277bce0b
polarjack <luckyjack85@gmail.com>
2017-07-19 17:15:53 +0800

    init

---
+++ index.js
@@ -0,0 +1,88 @@
+var cards = new Vue({
+	el: '#field',
+	data: {
+		cards: [{
+			inputWord: '',
+			mode: 'include',
+			fromDate: '',
+			toDate: '',
+			words: [],
+			level: {
+				type: 'literati',
+				name: '',
+				mode: ''
+			},
+			toselect: '',
+			options: [
+				'正國級',
+				'副國級',
+				'正省部級',
+				'副省部級',
+				'正廳局級',
+				'副廳局級',
+				'正縣處級',
+				'副縣處級',
+				'正科級',
+				'副科級'
+			]
+		}]
+	},
+	methods: {
+		addWord: function (cindex) {
+			this.cards[cindex].words.push({
+				content: this.cards[cindex].inputWord
+			})
+			this.cards[cindex].inputWord = ''
+			console.log(cindex)
+		},
+		addCard: function () {
+			this.cards.push({
+				inputWord: '',
+				mode: 'include',
+				fromDate: '',
+				toDate: '',
+				words: []
+			})
+		},
+		deleteCard: function (cindex) {
+			this.cards.splice(cindex, 1)
+		},
+		deleteWord: function (cindex, windex) {
+			this.cards[cindex].words.splice(windex, 1)
+		},
+		changeMode: function (cindex, mode) {
+			this.cards[cindex].mode = mode
+		},
+		changeLevelName: function(cindex) {
+			this.cards[cindex].level.name = this.cards[cindex].toselect
+		},
+		changeLevelMode: function(cindex, mode) {
+			this.cards[cindex].level.mode = mode
+		},
+		showData: function() {
+			console.log(this.cards)
+		}
+	}
+})
+
+jQuery(function () {
+	jQuery('#submit-button').on('click', function () {
+		window.localStorage.setItem('card-search-cards', JSON.stringify({
+			cards: cards.cards.map(function (card) {
+				return {
+					mode: card.mode,
+					fromDate: card.fromDate,
+					toDate: card.toDate,
+					words: card.words.map(function (word) {
+						return word.content
+					})
+				}
+			})
+		}))
+
+	//window.location.href = window.location.origin + '/result.html'
+	// var tmp = window.location.origin
+	var tmp = "http://cped2.nccu.edu.tw:1081"
+    window.open(tmp + '/result.html', '_blank')
+  })
+})
---
+++ result.js
@@ -0,0 +1,109 @@
+function POST(url, data, callback, failback) {
+  return jQuery
+    .ajax(url, {
+      method: 'POST',
+      cache: false,
+      data: data,
+      crossDomain: true
+    })
+    .done(callback)
+    .fail(failback)
+}
+
+var people = new Vue({
+  el: '#table-body',
+  data: {
+    allselected: false,
+    people: []
+  },
+  methods: {
+    selectAllOrNot: function () {
+      var self = this;
+
+      if (self.allselected === false) {
+        self.people.forEach(i => {
+          i.checked = true
+        });
+      } else {
+        self.people.forEach(i => {
+          i.checked = false;
+        });
+      }
+      self.allselected = !self.allselected;
+    },
+    exportData: function () {
+      var self = this;
+
+      var BOM = '\uFEFF';
+
+      var firstRow = [BOM + '姓名,漢語拼音,出生地,專業頭銜,最高學歷,留學經驗,留學國家,現任職務,工作經歷,學習經歷,民族,出生,死亡,祖籍,政黨,入黨時間,共青團,資料庫連結,特殊關係'];
+
+      // unused code
+      // var otherRows = self.people
+      //   .filter(i => i.checked === true)
+      //   .map((i) => [i.name, i.pingyingname,
+      //     i.hometown, i.profession, i.degree, i.abroadexp, i.abroadcountry,
+      //     i.currentStatusSet.map((c) => {
+      //       return [c.startTime.concat("~@").concat(" ").concat(c.content)];
+      //     }).join(' || '),
+      //     i.careerHistorySet.map((c) => {
+      //       return c.startTime.concat("~@").concat(c.endTime).concat(" ").concat(c.content);
+      //     }).join(' || '),
+      //     i.studyHistorySet.map((c) => {
+      //       return c.startTime.concat("~@").concat(c.endTime).concat(" ").concat(c.content);
+      //     }).join(' || '),
+      //     i.race, i.birthday, i.alive, i.zuzhi, i.party, i.enterparty, i.gongchintuan, i.url, i.relation, i.other
+      //   ].join(','))
+
+      var coma = "\/" //prevent excel change the date to english
+      var otherRows = self.people
+        .filter(i => i.checked == true)
+        .map((i) => [i.name, i.pingyingname, i.hometown, i.profession, i.degree, i.abroadexp, i.abroadcountry,
+          i.currentStatusSet.map((c) => [c.startTime.concat("~@").concat(" ").concat(c.content)]).join(' || '),
+          i.careerHistorySet.map((c) => [c.startTime.concat("~").concat(c.endTime).concat(" ").concat(c.content)]).join(' || '),
+          i.studyHistorySet.map((c) => [c.startTime.concat("~").concat(c.endTime).concat(" ").concat(c.content)]).join(' || '),
+          i.race, i.birthday + coma, i.alive, i.zuzhi, i.party, i.enterparty, i.gongchintuan, i.url, i.relation
+        ].join(","))
+
+      var rows = firstRow.concat(otherRows)
+      console.log(rows)
+      generateBlobAndSave(rows, "下載資料")
+    }
+  }
+})
+
+function generateBlobAndSave(contentArray, exportInfo) {
+  var blob = new Blob(contentArray.map(function (line) {
+    return line + '\n';
+  }), {
+    type: 'text/csv; charset=utf-8',
+    endings: 'native'
+  });
+
+  var url = URL.createObjectURL(blob);
+
+  var a = document.createElement('a');
+  document.body.appendChild(a);
+  a.style = 'display: none';
+
+  a.href = url;
+  a.download = '中共菁英資料庫匯出' + Date.now() + '.csv';
+  a.click();
+
+  window.URL.revokeObjectURL(url);
+}
+
+jQuery(function () {
+  var cards = JSON.parse(window.localStorage.getItem('card-search-cards'))
+
+  POST('/cards', cards, function (res) {
+    people.people = res.map(function (r) {
+      r.checked = false
+      return r
+    })
+  })
+})
+
+function goBack() {
+  window.location.href = window.location.origin + '/index.html'
+}
